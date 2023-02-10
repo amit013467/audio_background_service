c2787e9ae5c35f27e658176beb5c0cace001c5d2
amit013467 <120100999+amit013467@users.noreply.github.com>
2023-02-10 15:26:56 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/audio_background_service/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.audio_background_service
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs
+
+// FOR MORE EXAMPLES, VISIT THE GITHUB REPOSITORY AT:
+//
+//  https://github.com/ryanheise/audio_service
+//
+// This example implements a minimal audio handler that renders the current
+// media item and playback state to the system notification and responds to 4
+// media actions:
+//
+// - play
+// - pause
+// - seek
+// - stop
+//
+// To run this example, use:
+//
+// flutter run
+
+import 'dart:async';
+
+import 'package:audio_service/audio_service.dart';
+import '/common.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
+
+// You might want to provide this using dependency injection rather than a
+// global variable.
+late AudioHandler audioHandler;
+
+Future<void> main() async {
+  audioHandler = await AudioService.init(
+    builder: () => AudioPlayerHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'com.ryanheise.myapp.channel.audio',
+      androidNotificationChannelName: 'Audio playback',
+      androidNotificationOngoing: true,
+    ),
+  );
+  runApp(const MyApp());
+}
